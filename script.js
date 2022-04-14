c092615d59ad98dc77583c139ac6d6bb22454e04
tessie475 <eberechichukwu1@gmail.com>
2022-04-14 13:06:55 -0700

    modified index.html and created the js file

---
+++ script.js
@@ -0,0 +1,5 @@
+const submit = document.querySelector('button')
+
+document.querySelector('button').addEventListener('click', function() {
+    submit.style.backgroundColor = 'hsl(213, 19%, 18%)';
+})
